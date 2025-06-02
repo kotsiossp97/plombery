fff7c5a195b5b850c9604cbd2f29792879d1e2b5
Konstantinos Andreou <kotsiossp@gmail.com>
2025-06-02 09:39:00 +0300

    Add replace_existing parameter to job scheduling for better management

--- src/plombery/orchestrator/__init__.py
+++ src/plombery/orchestrator/__init__.py
@@ -54,6 +54,7 @@ class _Orchestrator:
                 # Jobs will be run even if they arrive 1 min late
                 misfire_grace_time=timedelta(minutes=settings.misfire_grace_mins).seconds,
                 max_instances=settings.scheduler_job_max_instances,
+                replace_existing=True,
             )
 
     def get_pipeline(self, pipeline_id: str):
@@ -124,6 +125,7 @@ async def run_pipeline_now(
             max_instances=settings.scheduler_job_max_instances,
             misfire_grace_time=None,
             trigger=DateTrigger(),
+            replace_existing=True,
         ),
         [datetime.now()],
     )
